2e56d763e303265ad4bbac7c6089e638eee78282
Andreas Dangel <andreas.dangel@microdoc.com>
2019-02-22 14:57:09 +0100

    Avoid deprecated property builder

--- pmd-plsql-custom/src/main/java/net/sourceforge/pmd/examples/plsql/rules/ShortVariableRule.java
+++ pmd-plsql-custom/src/main/java/net/sourceforge/pmd/examples/plsql/rules/ShortVariableRule.java
@@ -7,15 +7,16 @@ package net.sourceforge.pmd.examples.plsql.rules;
 import net.sourceforge.pmd.lang.plsql.ast.ASTID;
 import net.sourceforge.pmd.lang.plsql.ast.ASTVariableOrConstantDeclaratorId;
 import net.sourceforge.pmd.lang.plsql.rule.AbstractPLSQLRule;
-import net.sourceforge.pmd.properties.IntegerProperty;
+import net.sourceforge.pmd.properties.PropertyDescriptor;
+import net.sourceforge.pmd.properties.PropertyFactory;
+import net.sourceforge.pmd.properties.constraints.NumericConstraints;
 
 public class ShortVariableRule extends AbstractPLSQLRule {
 
-    private static final IntegerProperty MINIMUM_LENGTH = IntegerProperty.named("minimumLength")
+    private static final PropertyDescriptor<Integer> MINIMUM_LENGTH = PropertyFactory.intProperty("minimumLength")
             .desc("Number of characters that are required as a minimum for a variable name.")
             .defaultValue(3)
-            .range(1, 100)
-            .uiOrder(1.0f)
+            .require(NumericConstraints.inRange(1, 100))
             .build();
 
     public ShortVariableRule() {
