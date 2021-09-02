153d7a8b8c00a1c598b4dccb15a8f651f35d49ec
Andreas Dangel <andreas.dangel@pmd-code.org>
2021-09-02 10:21:13 +0200

    Add example for a simple maven based project

---
+++ maven/simple-project/src/main/java/net/sourceforge/pmd/examples/maven/simpleproject/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    // UnusedPrivateField
+    private String unusedField;
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        // EmptyIfStmt
+        if (args.length == 0);
+    }
+}
