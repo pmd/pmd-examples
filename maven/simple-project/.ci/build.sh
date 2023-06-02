2cfd612697899523e3a398a602848167a490f897
Andreas Dangel <andreas.dangel@pmd-code.org>
2023-06-02 17:09:03 +0200

    [ci] Improve caches, reduce command outputs

--- maven/simple-project/.ci/build.sh
+++ maven/simple-project/.ci/build.sh
@@ -2,10 +2,8 @@
 
 # exit immediately if a command fails
 set -e
-java -version
-mvn --version
 
-mvn verify | tee build.log
+mvn --show-version --batch-mode verify | tee build.log
 grep "You have 2 PMD violations" build.log || (echo -e "\n\n\x1b[31mMissing expected rule violation\e[0m"; exit 1)
 
 echo -e "\n\n\x1b[32mTest successful\e[0m"
